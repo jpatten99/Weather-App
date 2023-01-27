8f606bfe1ba4a8511289e2d1c79d63d5d4e880de
Joshua <joshuagpatten@gmail.com>
2023-01-26 20:14:41 -0500

    Add render data function, Add some DOM structure

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
+
+
 const searchCity = () => {
-  console.log("click");
-  let city = document.getElementById("cityInput");
-  let value = city.value;
-  // console.log(city.value);
-  city.value= "";
+  let input = document.getElementById("cityInput");
+  let value = input.value;
+  input.value= "";
   return value;
 }
+
+const renderData = (filteredWeatherData) => {
+  document.getElementById("cityName").innerText = "City Name: " + filteredWeatherData.cityName;
+  document.getElementById("temperature").innerText = "Current Temperature: " + filteredWeatherData.temperature;
+  document.getElementById("feelsLike").innerText = "Feels Like: " + filteredWeatherData.feelsLike;
+  document.getElementById("windSpeed").innerText = "Wind Speed: " + filteredWeatherData.windSpeed;
+  document.getElementById("minTemp").innerText = "Minimum Temperature: " + filteredWeatherData.minTemp;
+  document.getElementById("maxTemp").innerText = "Maximum Temperature: " + filteredWeatherData.maxTemp;
+}
+
 
 async function getData(city) {
 
   try{
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=07fd1335c6d1a5773a284f1a9ba67c10`, {mode: 'cors'});
-    const weatherData = await response.json();
-    console.log(weatherData);
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=07fd1335c6d1a5773a284f1a9ba67c10&units=imperial`, {mode: 'cors'});
+    let weatherData = await response.json();
+    // console.log(weatherData);
+    let filteredWeatherData = {
+      cityName: weatherData.name,
+      temperature: Math.round(weatherData.main.temp),
+      feelsLike: Math.round(weatherData.main.feels_like),
+      maxTemp: Math.round(weatherData.main.temp_max),
+      minTemp: Math.round(weatherData.main.temp_min),
+      windSpeed: Math.round(weatherData.wind.speed)
+    }
+    renderData(filteredWeatherData);
+    // console.log(filteredWeatherData);
   } catch (err) {
     console.log(err);
   }
 }
 
+
 document.getElementById("searchButton").addEventListener("click", ()=>{
   getData(searchCity());
+  
 });
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=toledo,OH,US&APPID=07fd1335c6d1a5773a284f1a9ba67c10&units=imperial`, {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    // console.log(response);
+    let filteredWeatherData = {
+      cityName: response.name,
+      temperature: Math.round(response.main.temp),
+      feelsLike: Math.round(response.main.feels_like),
+      maxTemp: Math.round(response.main.temp_max),
+      minTemp: Math.round(response.main.temp_min),
+      windSpeed: Math.round(response.wind.speed)
+    }
+    renderData(filteredWeatherData);
+  })
+  .catch(function(err) {
+    // Error :(
+  });
