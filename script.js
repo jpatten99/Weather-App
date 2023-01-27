fa5a34baeaa8173acfff9bcfd1b6d01e9b553f67
Joshua <joshuagpatten@gmail.com>
2023-01-27 15:46:56 -0500

    Add unit change functionality

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
 
+var temperatureUnits = "*F";
+var weatherData = {};
+
+const convertTemps = (buttonUnits) => {
+  if(buttonUnits != temperatureUnits){
+    var temp = weatherData.temperature;
+    console.log("Temp " + Math.round(temp));
+    var feelsLike = weatherData.feelsLike;
+    console.log("feelsLike " + Math.round(feelsLike));
+    var minTemp = weatherData.minTemp;
+    console.log("mintemp " + Math.round(minTemp));
+    var maxTemp = weatherData.maxTemp;
+    console.log("maxtemp " + Math.round(maxTemp));
+    if(buttonUnits=="*C"){
+      document.getElementById("celsius").classList.add("current");
+      document.getElementById("fahrenheit").classList.remove("current");
+      temp = ((temp-32) * (5/9));
+      weatherData.temperature = temp;
+      document.getElementById("temperature").innerText = "Temp: " + Math.round(temp) + buttonUnits + "<->";
+      feelsLike = ((feelsLike-32) * (5/9));
+      weatherData.feelsLike = feelsLike;
+      document.getElementById("feelsLike").innerText = "Feels Like: " + Math.round(feelsLike) + buttonUnits + "<->";
+      minTemp = ((minTemp-32) * (5/9));
+      weatherData.minTemp = minTemp;
+      document.getElementById("minTemp").innerText = "Min Temp: " + Math.round(minTemp) + buttonUnits;
+      maxTemp = ((maxTemp-32) * (5/9));
+      weatherData.maxTemp = maxTemp;
+      document.getElementById("maxTemp").innerText = "Max Temp: " + Math.round(maxTemp) + buttonUnits;
+      temperatureUnits = "*C";
+    }
+    else{
+      document.getElementById("fahrenheit").classList.add("current");
+      document.getElementById("celsius").classList.remove("current");
+      temp = ((temp *(1.8)) + 32);
+      weatherData.temperature = temp;
+      document.getElementById("temperature").innerText = "Temp: " + Math.round(temp) + buttonUnits + "<->";
+      feelsLike = ((feelsLike *(1.8)) + 32);
+      weatherData.feelsLike = feelsLike;
+      document.getElementById("feelsLike").innerText = "Feels Like: " + Math.round(feelsLike) + buttonUnits + "<->";
+      minTemp = ((minTemp *(1.8)) + 32);
+      weatherData.minTemp = minTemp;
+      document.getElementById("minTemp").innerText = "Min Temp: " + Math.round(minTemp) + buttonUnits;
+      maxTemp = ((maxTemp *(1.8)) + 32);
+      weatherData.maxTemp = maxTemp;
+      document.getElementById("maxTemp").innerText = "Max Temp: " + Math.round(maxTemp) + buttonUnits;
+      temperatureUnits = "*F";
+    }
+  }
+}
 
 const searchCity = () => {
   let input = document.getElementById("cityInput");
@@ -7,32 +56,42 @@ const searchCity = () => {
   return value;
 }
 
-const renderData = (filteredWeatherData) => {
-  document.getElementById("cityName").innerText = filteredWeatherData.cityName;
-  document.getElementById("temperature").innerText = "Temp: " + filteredWeatherData.temperature + "<->";
-  document.getElementById("feelsLike").innerText = "Feels Like: " + filteredWeatherData.feelsLike + "<->";
-  document.getElementById("windSpeed").innerText = "Wind Speed: " + filteredWeatherData.windSpeed;
-  document.getElementById("minTemp").innerText = "Min Temp: " + filteredWeatherData.minTemp;
-  document.getElementById("maxTemp").innerText = "Max Temp: " + filteredWeatherData.maxTemp;
+const renderData = (weatherData) => {
+  document.getElementById("cityName").innerText = weatherData.cityName;
+  document.getElementById("temperature").innerText = "Temp: " + Math.round(weatherData.temperature) + temperatureUnits + "<->";
+  document.getElementById("feelsLike").innerText = "Feels Like: " + Math.round(weatherData.feelsLike) + temperatureUnits + "<->";
+  document.getElementById("windSpeed").innerText = "Wind Speed: " + Math.round(weatherData.windSpeed) + "mph";
+  document.getElementById("minTemp").innerText = "Min Temp: " + Math.round(weatherData.minTemp) + temperatureUnits;
+  document.getElementById("maxTemp").innerText = "Max Temp: " + Math.round(weatherData.maxTemp) + temperatureUnits;
 }
 
 
 async function getData(city) {
 
   try{
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=07fd1335c6d1a5773a284f1a9ba67c10&units=imperial`, {mode: 'cors'});
-    let weatherData = await response.json();
-    // console.log(weatherData);
-    let filteredWeatherData = {
-      cityName: weatherData.name,
-      temperature: Math.round(weatherData.main.temp),
-      feelsLike: Math.round(weatherData.main.feels_like),
-      maxTemp: Math.round(weatherData.main.temp_max),
-      minTemp: Math.round(weatherData.main.temp_min),
-      windSpeed: Math.round(weatherData.wind.speed)
+    let response;
+    if(temperatureUnits == "*F"){
+      response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=07fd1335c6d1a5773a284f1a9ba67c10&units=imperial`, {mode: 'cors'});
     }
-    renderData(filteredWeatherData);
-    // console.log(filteredWeatherData);
+    else{
+      response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=07fd1335c6d1a5773a284f1a9ba67c10&units=metric`, {mode: 'cors'});
+    }
+    let DATA = await response.json();
+    // console.log(weatherData);
+    weatherData.cityName = DATA.name;
+    weatherData.temperature = DATA.main.temp;
+    weatherData.feelsLike = DATA.main.feels_like;
+    weatherData.maxTemp =  DATA.main.temp_max;
+    weatherData.minTemp = DATA.main.temp_min;
+    if(temperatureUnits == "*C"){
+      weatherData.windSpeed = (DATA.wind.speed *  2.2369);
+    }
+    else{
+      weatherData.windSpeed = DATA.wind.speed;
+    }
+    
+    renderData(weatherData);
+
   } catch (err) {
     console.log(err);
   }
@@ -44,23 +103,23 @@ document.getElementById("searchButton").addEventListener("click", ()=>{
   
 });
 
+// document.getElementById("celsius").addEventListener('click', ()=>{convertTemps(this.value)});
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=toledo,OH,US&APPID=07fd1335c6d1a5773a284f1a9ba67c10&units=imperial`, {mode: 'cors'})
   .then(function(response) {
     return response.json();
   })
   .then(function(response) {
-    // console.log(response);
-    let filteredWeatherData = {
-      cityName: response.name,
-      temperature: Math.round(response.main.temp),
-      feelsLike: Math.round(response.main.feels_like),
-      maxTemp: Math.round(response.main.temp_max),
-      minTemp: Math.round(response.main.temp_min),
-      windSpeed: Math.round(response.wind.speed)
-    }
-    renderData(filteredWeatherData);
+    weatherData.cityName = response.name;
+    weatherData.temperature = response.main.temp;
+    weatherData.feelsLike = response.main.feels_like;
+    weatherData.maxTemp =  response.main.temp_max;
+    weatherData.minTemp = response.main.temp_min;
+    weatherData.windSpeed = response.wind.speed;
+    renderData(weatherData);
   })
   .catch(function(err) {
     // Error :(
-  });
+  }
+);
+
